c0ede300f487f6d35e2dbf99e5ded32bca6821b6
hilquiasfmelo <hilquiasfmelo@hotmail.com>
2024-02-14 19:33:34 -0300

    Cadastro de restaurante

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,7 @@
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 
+import { Toaster } from './components/ui/sonner'
 import { router } from './routes'
 
 export function App() {
@@ -8,6 +9,7 @@ export function App() {
     <HelmetProvider>
       <Helmet titleTemplate="%s | pizza.shop" />
       <RouterProvider router={router} />
+      <Toaster richColors />
     </HelmetProvider>
   )
 }
