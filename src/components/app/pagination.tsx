ad9db57832a8bf34774ece20a31c5de4085cea08
Hilquias Ferreira Melo <hilquiasfmelo@hotmail.com>
2024-02-16 10:20:17 -0300

    Paginação completa

---
+++ src/components/app/pagination.tsx
@@ -0,0 +1,63 @@
+import {
+  ChevronLeft,
+  ChevronRight,
+  ChevronsLeft,
+  ChevronsRight,
+} from 'lucide-react'
+
+import { Button } from '../ui/button'
+
+export interface PaginationProps {
+  // página inicial = 0
+  pageIndex: number
+  // total de registros
+  totalCount: number
+  // números de registros por página ou número total de páginas
+  perPage: number
+}
+
+export function Pagination({
+  pageIndex,
+  totalCount,
+  perPage,
+}: PaginationProps) {
+  /**
+   * Math.ceil => Arredonda o cálculo sempre pra cima. Ex: 1.5 => 2
+   * (totalCount / perPage) || 1 => retorna o total de páginas tem
+   * conforme o número de registos, senão retorna só uma página
+   */
+  const pages = Math.ceil(totalCount / perPage) || 1
+
+  return (
+    <div className="flex items-center justify-between">
+      <span className="text-sm text-muted-foreground">
+        Total de {totalCount} item(s)
+      </span>
+
+      <div className="flex items-center gap-6 lg:gap-8">
+        <div className="text-sm font-medium">
+          Página {pageIndex + 1} de {pages}
+        </div>
+
+        <div className="flex items-center gap-2">
+          <Button variant="outline" className="h-8 w-8 p-0">
+            <ChevronsLeft className="h-4 w-4" />
+            <span className="sr-only">Primeira página</span>
+          </Button>
+          <Button variant="outline" className="h-8 w-8 p-0">
+            <ChevronLeft className="h-4 w-4" />
+            <span className="sr-only">Página anterior</span>
+          </Button>
+          <Button variant="outline" className="h-8 w-8 p-0">
+            <ChevronRight className="h-4 w-4" />
+            <span className="sr-only">Próxima página</span>
+          </Button>
+          <Button variant="outline" className="h-8 w-8 p-0">
+            <ChevronsRight className="h-4 w-4" />
+            <span className="sr-only">Última página</span>
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
+}
