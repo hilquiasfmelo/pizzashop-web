4d04728a5431199199131c3094ddab4ea30ea8b0
Hilquias Ferreira Melo <hilquiasfmelo@hotmail.com>
2024-02-16 15:50:54 -0300

    Dashboard

--- src/components/app/account-menu.tsx
+++ src/components/app/account-menu.tsx
@@ -39,7 +39,7 @@ export function AccountMenu() {
           <Building className="mr-2 h-4 w-4" />
           <span>Perfil da loja</span>
         </DropdownMenuItem>
-        <DropdownMenuItem className="text-rose-500 dark:text-rose-600">
+        <DropdownMenuItem className="text-rose-600 dark:text-rose-500">
           <LogOut className="mr-2 h-4 w-4" />
           <span>Sair</span>
         </DropdownMenuItem>
