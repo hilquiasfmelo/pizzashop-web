57c58fbbf046dfec7f81a65978a3e032e589ad34
hilquiasfmelo <hilquiasfmelo@hotmail.com>
2024-02-20 21:07:57 -0300

    Autenticação do usuário

---
+++ src/api/sign-in.ts
@@ -0,0 +1,11 @@
+import { API } from '@/lib/axios'
+
+export interface SignInBody {
+  email: string
+}
+
+export async function signIn({ email }: SignInBody) {
+  await API.post('/authenticate', {
+    email,
+  })
+}
